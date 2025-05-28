13365908dfff2357fe6fd1987d625e0927ae0fd3
vorning <82236004+vorning@users.noreply.github.com>
2025-05-28 15:07:37 +0200

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/", // vigtigt!
+  base: "/tankebobler/", // vigtigt!
   plugins: [react()],
 });
